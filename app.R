62f11b92ed4f433cb01cb7fb67ff1da9bad52af7
nbhabish <bhabishya.neupane@huskers.unl.edu>
2022-01-25 18:05:14 -0600

    initial commit

---
+++ app.R
@@ -0,0 +1,385 @@
+# Libraries ----
+
+library(shiny)
+library(shinyWidgets)
+library(shinythemes)
+library(shinyjs)
+
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+library(modeltime)
+
+library(bslib)
+library(thematic)
+
+shinyOptions(bootstrapLib = TRUE)
+thematic::thematic_shiny(font = "auto")
+
+my_theme <- bslib::bs_theme(
+  version    = 4,
+  bootswatch = "flatly",
+  fg         = "#1C3516",
+  bg         = "white",
+  base_font  = font_google("Rancho"),
+  heading_font = font_google("Rancho"),
+  font_scale  = 1,
+  primary = "#4285F4",
+  secondary = "#4285F4",
+  success = "black"
+)
+
+
+forecasted_data <- read_rds("00_data/forecast_data.rds")
+
+models_renamed <- forecasted_data %>%
+  mutate(
+    .model_desc = case_when(
+      .model_desc == "ARIMA(2,1,0)(4,1,1)[12]" ~ "ARIMA",
+      .model_desc == "ETS(A,A,A)" ~ "ETS",
+      .model_desc == "SEASONAL DECOMP: ETS(A,N,N)" ~ "ETS: SEASONAL DECOMPOSITION",
+      .model_desc == "UPDATE: TBATS(0.714, {0,0}, -, {<12,5>, <24,1>, <36,1>})" ~ "TBATS",
+      .model_desc == "SEASONAL DECOMP: ARIMA(4,1,1) WITH DRIFT" ~ "ARIMA: SEASONAL DECOMPOSITION",
+      .model_desc == "UPDATE: REGRESSION WITH ARIMA(0,0,1)(0,1,1)[12] ERRORS" ~ "AUTO ARIMA",
+      .model_desc == "PROPHET" ~ "PROPHET",
+      .model_desc == "ACTUAL" ~ "ACTUAL"
+    )
+  )
+
+residual_in_sample_acf <-
+  read_rds("00_data/residuals_in_sample_tbl.rds") %>%
+  mutate(
+    .model_desc = case_when(
+      .model_desc == "ARIMA(2,1,0)(4,1,1)[12]" ~ "ARIMA",
+      .model_desc == "ETS(A,A,A)" ~ "ETS",
+      .model_desc == "SEASONAL DECOMP: ETS(A,N,N)" ~ "ETS: SEASONAL DECOMPOSITION",
+      .model_desc == "TBATS(0.282, {2,1}, -, {<12,5>, <24,1>, <36,1>})" ~ "TBATS",
+      .model_desc == "SEASONAL DECOMP: ARIMA(4,1,1) WITH DRIFT" ~ "ARIMA: SEASONAL DECOMPOSITION",
+      .model_desc == "REGRESSION WITH ARIMA(1,1,1)(2,1,1)[12] ERRORS" ~ "AUTO ARIMA",
+      .model_desc == "PROPHET" ~ "PROPHET",
+      .model_desc == "ACTUAL" ~ "ACTUAL"
+    )
+  )
+residual_in_sample_acf %>% 
+  distinct(.model_desc)
+
+residual_in_sample_acf %>% 
+  filter(.model_desc %in% c("ETS: SEASONAL DECOMPOSITION", "ACTUAL")) %>% plot_modeltime_residuals()
+
+residual_in_sample_acf %>% 
+  distinct(.model_desc)
+
+residual_out_of_sample_acf <-
+  read_rds("00_data/residuals_out_of_sample_tbl.rds") %>%
+  mutate(
+    .model_desc = case_when(
+      .model_desc == "ARIMA(2,1,0)(4,1,1)[12]" ~ "ARIMA",
+      .model_desc == "ETS(A,A,A)" ~ "ETS",
+      .model_desc == "SEASONAL DECOMP: ETS(A,N,N)" ~ "ETS: SEASONAL DECOMPOSITION",
+      .model_desc == "TBATS(0.282, {2,1}, -, {<12,5>, <24,1>, <36,1>})" ~ "TBATS",
+      .model_desc == "SEASONAL DECOMP: ARIMA(4,1,1) WITH DRIFT" ~ "ARIMA: SEASONAL DECOMPOSITION",
+      .model_desc == "REGRESSION WITH ARIMA(1,1,1)(2,1,1)[12] ERRORS" ~ "AUTO ARIMA",
+      .model_desc == "PROPHET" ~ "PROPHET",
+      .model_desc == "ACTUAL" ~ "ACTUAL"
+    )
+  )
+
+
+#  UI ----
+
+ui <- navbarPage(
+  title = h2("United States Natural Gas Demand Forecast"),
+  inverse = FALSE,
+  collapsible = TRUE,
+  theme = my_theme,
+  
+  tags$body(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+  
+  
+  shinyjs::useShinyjs(),
+  
+  # Application UI ----
+  
+  # * Input Section -----
+  
+  # TabPanel 1 ----
+  tabPanel(
+    title = h4("Forecast"),
+    
+    
+    div(class = "container",
+        id    = "application_ui",
+        fluidRow(
+          column(
+            width = 3,
+            wellPanel(
+              div(
+                id = "main_input",
+                pickerInput(
+                  inputId = "models",
+                  label   = h2("Models"),
+                  choices = c(
+                    "ARIMA",
+                    "ARIMA: SEASONAL DECOMPOSITION",
+                    "AUTO ARIMA",
+                    "ETS",
+                    "ETS: SEASONAL DECOMPOSITION",
+                    "PROPHET",
+                    "TBATS"
+                  ),
+                  multiple = FALSE,
+                  selected = "ALL",
+                  option   = pickerOptions(
+                    actionsBox = FALSE,
+                    liveSearch = TRUE,
+                    size       = 5
+                  )
+                )
+              ),
+              div(
+                id = "input_buttons",
+                actionButton(
+                  inputId = "apply",
+                  label = "Apply",
+                  icon = icon("play")
+                ),
+                div(
+                  class = "pull-right",
+                  actionButton(
+                    inputId = "reset",
+                    label = "Reset",
+                    icon = icon("sync")
+                  )
+                )
+              )
+            ),
+            
+            br(),
+            br(),
+            
+            div(
+              class = "well",
+              h3("About the Data"),
+              id = "lorem_ipsum",
+              p(tags$small(
+                "The data comes from",code("USgas"), "R package. It provides an overview of demand 
+                for natural gas in the US in a time-series format.",
+                a(
+                  class = "btn btn-primary btn-sm",
+                  href = "https://github.com/RamiKrispin/USgas",
+                  target = "_blank",
+                  "Learn More"
+                )
+              )),
+            )
+            
+          ),
+          column(width = 9,
+                 div(
+                   class = "panel",
+                   div(
+                     class = "panel-body",
+                     style = "padding: 20px;",
+                     plotlyOutput(outputId = "plot_forecast"),
+                   )
+                 ))
+        ))
+  ),
+  tabPanel(
+    
+    # Phase Awards ----
+    
+    title = h4("Post-Forecast Diagnostics"),
+    
+    div(class = "container",
+        id    = "application_ui",
+        fluidRow(
+          column(
+            width = 3,
+            wellPanel(div(
+              id = "main_input",
+              pickerInput(
+                inputId = "models_2",
+                label   = h2("Models"),
+                choices = c(
+                  "ARIMA",
+                  "ARIMA: SEASONAL DECOMPOSITION",
+                  "AUTO ARIMA",
+                  "ETS",
+                  "ETS: SEASONAL DECOMPOSITION",
+                  "PROPHET",
+                  "TBATS"
+                ),
+                multiple = FALSE,
+                selected = "ALL",
+                option   = pickerOptions(
+                  actionsBox = FALSE,
+                  liveSearch = TRUE,
+                  size       = 5
+                )
+              ),
+              div(
+                id = "input_buttons",
+                actionButton(
+                  inputId = "apply_1",
+                  label = "Apply",
+                  icon = icon("play")
+                ),
+                div(
+                  class = "pull-right",
+                  actionButton(
+                    inputId = "reset_1",
+                    label = "Reset",
+                    icon = icon("sync")
+                  )
+                ),
+                
+              )
+            )),
+            
+            # Line breaks ----
+            br(),
+            br(),
+            
+            div(
+              class = "well",
+              h3("About the Data"),
+              id = "lorem_ipsum",
+              p(tags$small(
+                "The data comes from",code("USgas"), "R package. It provides an overview of demand 
+                for natural gas in the US in a time-series format.",
+                a(
+                  class = "btn btn-primary btn-sm",
+                  href = "https://github.com/RamiKrispin/USgas",
+                  target = "_blank",
+                  "Learn More"
+                )
+              )),
+            )
+          ),
+          column(width = 9,
+                 div(
+                   class = "panel",
+                   div(
+                     class = "panel-body",
+                     style = "padding: 20px;",
+                     tabsetPanel(
+                       type = "tabs",
+                       
+                       tabPanel(title = "In-Sample Residuals Plot",
+                                plotlyOutput(outputId = "plotly_residuals_in_sample")),
+                       
+                       tabPanel(title = "Out-of-Sample Residuals Plot",
+                                plotlyOutput(outputId = "plotly_residuals_out_of_sample")),
+                       
+                       tabPanel(title = "In-Sample Residuals ACF Plot",
+                                plotlyOutput(outputId = "plotly_residuals_acf_in_sample")),
+                       
+                       tabPanel(title = "Out-of-Sample Residuals ACF Plot",
+                                plotlyOutput(outputId = "plotly_residuals_acf_out_of_sample"))
+                     )
+                   )
+                   
+                 ))
+        ))
+  )
+)
+
+# Server ----
+
+server <- function(input, output, session) {
+  # bs_themer()
+  
+  # Server-side for tabPanel 1 ----
+  
+  observeEvent(eventExpr = input$reset, handlerExpr = {
+    updatePickerInput(session = session,
+                      inputId = "models",
+                      selected = "ARIMA")
+    
+    
+    shinyjs::delay(ms = 300, expr = {
+      shinyjs::click(id = "apply")
+    })
+    
+  })
+  
+  
+  models_renamed_reactive <-
+    eventReactive(eventExpr = input$apply, {
+      models_renamed %>%
+        filter(.model_desc %in% c(input$models, "ACTUAL"))
+      
+    }, ignoreNULL = FALSE)
+  
+  output$plot_forecast <- renderPlotly({
+    models_renamed_reactive() %>%
+      plot_modeltime_forecast(.title = "Tracking United States Monthly Gas Demand Until 2023")
+    
+  })
+  
+  
+  # Server-side for tabPanel 2 ----
+  
+  observeEvent(eventExpr = input$reset_1, handlerExpr = {
+    updatePickerInput(session = session,
+                      inputId = "models_2",
+                      selected = "ARIMA")
+    
+    shinyjs::delay(ms = 100, expr = {
+      shinyjs::click(id = "apply_1")
+    })
+    
+  })
+  
+  residuals_in_sample_acf <-
+    eventReactive(
+      eventExpr = input$apply_1,
+      valueExpr = {
+        residual_in_sample_acf %>%
+          filter(.model_desc %in% c(input$models_2, "ACTUAL"))
+        
+      },
+      ignoreNULL = FALSE
+    )
+  
+  residuals_out_of_sample_acf <-
+    eventReactive(
+      eventExpr = input$apply_1,
+      valueExpr = {
+        residual_out_of_sample_acf %>%
+          filter(.model_desc %in% c(input$models_2, "ACTUAL"))
+        
+      },
+      ignoreNULL = FALSE
+    )
+  
+  output$plotly_residuals_out_of_sample <- renderPlotly({
+    residuals_out_of_sample_acf() %>% 
+      plot_modeltime_residuals(.title = "")
+  })
+  
+  output$plotly_residuals_in_sample <- renderPlotly({
+    residuals_in_sample_acf() %>%
+      plot_modeltime_residuals(.title = "")
+  })
+  
+  output$plotly_residuals_acf_in_sample <- renderPlotly({
+    residuals_in_sample_acf() %>% 
+      plot_modeltime_residuals(.type = "acf",
+                               .title = "")
+  })
+  
+  output$plotly_residuals_acf_out_of_sample <- renderPlotly({
+    residuals_in_sample_acf() %>% 
+      plot_modeltime_residuals(.type = "acf",
+                               .title = "")
+  })
+  
+  
+}
+
+
+shinyApp(ui = ui, server = server)
